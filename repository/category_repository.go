7116cffb4ec514ce1ad3fc333a315ba66a502080
Laksa Bayu <laksabayu18.com>
2023-12-06 16:42:37 +0700

    add comment

--- repository/category_repository.go
+++ repository/category_repository.go
@@ -8,7 +8,7 @@ import (
 
 type CategoryRepository interface {
 	Save(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category //dikasih return
-	Update(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category
+	Update(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category//(domain.Category)-> mengembalikan data category nya
 	Delete(ctx context.Context, tx *sql.Tx, category domain.Category)
 	FindById(ctx context.Context, tx *sql.Tx, categoryId int) (domain.Category, error)
 	FindAll(ctx context.Context, tx *sql.Tx) []domain.Category
