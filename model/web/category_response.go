9faacdc699bdede3f063a74a96e6b38158721f97
Laksa Bayu <laksabayu18.com>
2023-12-10 03:20:26 +0700

    add comment again

--- model/web/category_response.go
+++ model/web/category_response.go
@@ -1,6 +1,6 @@
 package web
 
-type CategoryResponse struct {
+type CategoryResponse struct {//isinya cuma id dan name karena mengikuti apispec.json untuk response nya
 	Id   int    `json:"id"`
 	Name string	`json:"name"`
 }
