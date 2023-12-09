bc38b7fc9c59b5e6c021a21127c02228634cdfdd
Laksa Bayu <laksabayu18.com>
2023-12-10 03:18:46 +0700

    add comment again

--- helper/model.go
+++ helper/model.go
@@ -5,14 +5,16 @@ import (
 	"golang-restfullapi/model/web"
 )
 
-func ToCategoryResponse(category domain.Category) web.CategoryResponse {
+//untuk findById
+func ToCategoryResponse(category domain.Category) web.CategoryResponse {//function ini hanya berfungsi untuk mengembalikan response
 	return web.CategoryResponse{
 		Id: category.Id,
 		Name: category.Name,
 	}
 }
 
-func ToCategoryResponses(categories []domain.Category) []web.CategoryResponse {
+//untuk findAll
+func ToCategoryResponses(categories []domain.Category) []web.CategoryResponse {//function ini hanya berfungsi untuk mengembalikan response
 	var categoryResponse []web.CategoryResponse
 	for _, category := range categories {
 		categoryResponse = append(categoryResponse, ToCategoryResponse(category))
