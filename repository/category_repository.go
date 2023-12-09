7226ce71de44dcccb5c668c3870418f5770f5b4b
Laksa Bayu <laksabayu18.com>
2023-12-10 03:23:19 +0700

    add comment again

--- repository/category_repository.go
+++ repository/category_repository.go
@@ -7,6 +7,7 @@ import (
 )
 
 type CategoryRepository interface {
+	//Untuk parameternya biasakan kalau menggunakan golang itu selalu diawali dengan konteks 
 	Save(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category //dikasih return
 	Update(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category//(domain.Category)-> mengembalikan data category nya
 	Delete(ctx context.Context, tx *sql.Tx, category domain.Category)
